6237d6bf600a7a3965b42690229de6037f08e4c7
youngseo.kim <youngseo.kim@saltlux.com>
2024-01-23 17:36:18 +0900

    [2024-01-24 KYS]P8_뱀과사다리게임

--- src/newAlgorithm/P8_BJ16928.java
+++ src/newAlgorithm/P8_BJ16928.java
@@ -29,45 +29,73 @@ public class P8_BJ16928 {
             int to = Integer.parseInt(st.nextToken());
             routes.add(new Route(from, to));
         }
-
         bfs();
-
+//        dfs(1, 0);
         System.out.println(answer);
     }//main
 
+    /*
+        static void dfs(int start, int num) {
+            if (start == 100) {
+                answer = Math.min(num, answer);
+                return;
+            }
+            if (start > 100) return;
 
+            for (int i = 1; i <= 6; i++) {
+                int nStart = start + i;
+
+                if (nStart > 100) continue;
+                if (visit[nStart]) continue;
+                visit[nStart] = true;
+
+                for (Test.Route r : routes) {
+                    if (r.from == nStart) {
+                        dfs(r.to, num + 1);
+                        return; // 뱀이나 사다리를 만나면 해당 위치로 이동 후 종료
+                    }
+                }
+
+                dfs(nStart, num + 1);
+                visit[nStart] = false; // backtracking
+            }
+        }
+    */
     static void bfs() {
         q = new ArrayDeque();
         q.add(1);
-        visit[1]=true;
+        visit[1] = true;
 
         int num = 0;
-        while (!q.isEmpty() ) {
+        while (!q.isEmpty()) {
             int size = q.size();
             num++;
-            for (int i=0;i<size;i++) {
-                int start =q.poll();
-
+            for (int i = 0; i < size; i++) {
+                int start = q.poll();
 
                 if (start > 100) continue;
                 for (int j = 1; j < 7; j++) {
-                    int nStart = start+j;
-
+                    int nStart = start + j;
+                    //이동한 값이 100이면 최소값을 구해서 answer로 내놓기
                     if (nStart == 100) {
-                        answer = Math.min(num,answer);
+                        answer = Math.min(num, answer);
                         return;
                     }
 
-                    if(nStart>100)continue;
-                    if(visit[nStart])continue;
-                    visit[nStart]=true;
+                    if (nStart > 100) continue;
+                    if (visit[nStart]) continue;
+                    visit[nStart] = true;
 
+                    boolean moved = false;
                     for (Route r : routes) {
-                        if (r.from == nStart) {//사다리 만나면 무조건 타는데
+                        if (r.from == nStart) {//사다리나 뱀이 있으면 이동한 이후 값을 q에 넣는다.
                             q.add(r.to);
-                        } else {
-                            q.add(nStart);
+                            moved = true;
+                            break;
                         }
+                    }
+                    if (!moved) {//사다리나 뱀을 탈 게 없으면 주사위가 이동한 값을 q에 넣는다.
+                        q.add(nStart);
                     }
                 }//for_주사위
             }
@@ -84,14 +112,3 @@ public class P8_BJ16928 {
         }
     }//Route
 }//CLASS
-
-
-/*
-1 5
-2 92
-94 3
-95 4
-96 5
-97 6
-98 7
- */
--- src/newAlgorithm/Test.java
+++ src/newAlgorithm/Test.java
@@ -1,7 +1,69 @@
 package newAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("test");
+    static int N, M, answer;
+    static ArrayList<Route> routes;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        answer = Integer.MAX_VALUE;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visit = new boolean[101];
+        routes = new ArrayList<>();
+        for (int i = 0; i < N + M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            routes.add(new Route(from, to));
+        }
+
+        dfs(1, 0);
+        System.out.println(answer);
+    }
+
+    static void dfs(int start, int num) {
+        if (start == 100) {
+            answer = Math.min(num, answer);
+            return;
+        }
+        if (start > 100) return;
+
+        for (int i = 1; i <= 6; i++) {
+            int nStart = start + i;
+
+            if (nStart > 100) continue;
+            if (visit[nStart]) continue;
+            visit[nStart] = true;
+
+            for (Route r : routes) {
+                if (r.from == nStart) {
+                    dfs(r.to, num + 1);
+                    return; // 뱀이나 사다리를 만나면 해당 위치로 이동 후 종료
+                }
+            }
+
+            dfs(nStart, num + 1);
+            visit[nStart] = false; // backtracking
+        }
+    }
+
+    static class Route {
+        int from;
+        int to;
+
+        public Route(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
     }
 }
